d17cbbb29e635676f83c962e5a364b7c447b2975
mpetrun5 <matija.petrunic@gmail.com>
2024-04-10 15:10:54 +0200

    feat: add message ID field to more easily track message accross networks

--- relayer/message/message.go
+++ relayer/message/message.go
@@ -5,14 +5,16 @@ type Message struct {
 	Source      uint8       // Source where message was initiated
 	Destination uint8       // Destination chain of message
 	Data        interface{} // Data associated with the message
+	ID          string      // ID is used to track and identify message across networks
 	Type        MessageType // Message type
 }
 
-func NewMessage(source, destination uint8, data interface{}, msgType MessageType) *Message {
+func NewMessage(source, destination uint8, data interface{}, id string, msgType MessageType) *Message {
 	return &Message{
 		Source:      source,
 		Destination: destination,
 		Data:        data,
 		Type:        msgType,
+		ID:          id,
 	}
 }
