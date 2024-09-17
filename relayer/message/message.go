cdf07dc19782e45d7b41a5238b678c59e37e8489
mpetrun5 <matija.petrunic@gmail.com>
2024-09-17 16:38:54 +0200

    Allow for route function to return errors to message source

--- relayer/message/message.go
+++ relayer/message/message.go
@@ -7,14 +7,16 @@ type Message struct {
 	Data        interface{} // Data associated with the message
 	ID          string      // ID is used to track and identify message across networks
 	Type        MessageType // Message type
+	ErrChn      chan error  // ErrChn is used to share errors that happen on the destination handler
 }
 
-func NewMessage(source, destination uint8, data interface{}, id string, msgType MessageType) *Message {
+func NewMessage(source, destination uint8, data interface{}, id string, msgType MessageType, errChn chan error) *Message {
 	return &Message{
 		Source:      source,
 		Destination: destination,
 		Data:        data,
 		Type:        msgType,
 		ID:          id,
+		ErrChn:      errChn,
 	}
 }
