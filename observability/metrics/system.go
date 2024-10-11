2d3acf2c1f31f2232ce9498c98853cf2375e8c6c
mpetrun5 <matija.petrunic@gmail.com>
2024-10-11 16:29:30 +0200

    Add garbage cycle duration histogram

--- observability/metrics/system.go
+++ observability/metrics/system.go
@@ -3,12 +3,14 @@ package metrics
 import (
 	"context"
 	"runtime"
+	"runtime/debug"
 
 	"go.opentelemetry.io/otel/metric"
 )
 
 type SystemMetrics struct {
-	goRoutinesGauge metric.Int64ObservableGauge
+	goRoutinesGauge     metric.Int64ObservableGauge
+	gcDurationHistogram metric.Float64Histogram
 }
 
 // NewSystemMetrics initializes system performance and resource utilization metrics
@@ -25,7 +27,24 @@ func NewSystemMetrics(meter metric.Meter, opts metric.MeasurementOption) (*Syste
 		return nil, err
 	}
 
+	gcDurationHistogram, err := meter.Float64Histogram(
+		"relayer.GcDurationSeconds",
+		metric.WithFloat64Callback(func(context context.Context, result metric.Float64Observer) error {
+			var gcStats debug.GCStats
+			debug.ReadGCStats(&gcStats)
+			if len(gcStats.Pause) > 0 {
+				recentPauseDuration := gcStats.Pause[0].Seconds()
+				result.Observe(recentPauseDuration, opts)
+			}
+		}),
+		metric.WithDescription("Duration of garbage collection cycles."),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	return &SystemMetrics{
-		goRoutinesGauge: goRoutinesGauge,
+		goRoutinesGauge:     goRoutinesGauge,
+		gcDurationHistogram: gcDurationHistogram,
 	}, err
 }
